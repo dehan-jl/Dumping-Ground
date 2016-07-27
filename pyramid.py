24afe9dcca8458aedd6dabcf99c0eee45ff314ec
Dehan Jaco Lamprecht <dehan.lamprecht@gmail.com>
2016-07-27 13:32:02 +0200

    Less
    2 Lines less

--- pyramid.py
+++ pyramid.py
@@ -1,8 +1,6 @@
-height = int(input("height: "))
-for i in range(height):
-	for n in range(height-i):
-		print(" ", end='')
-	for j in range((2*i)-1):
-		print("#", end='')
+k = int(input())
+for i in range(k):
+	for n in range(k-i): print(" ", end='')
+	for j in range((2*i)-1): print("#", end='')
 	print()
 input()
