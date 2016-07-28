587bba93f337e35d03efb950bd676879d39251c2
Dehan Jaco Lamprecht <dehan.lamprecht@gmail.com>
2016-07-28 12:12:21 +0200

    Update full_diamond.py

--- full_diamond.py
+++ full_diamond.py
@@ -1,6 +1,4 @@
 k = int(input())
-for i in range(k+1):
-	print(" "*(k-i) + "#"*(2*i-1))
-for i in range(1, k+1):
-	print(" "*(i) + "#"*((2*k-1)-(2*i)))
+for i in range(k+1): print(" "*(k-i) + "#"*(2*i-1))
+for i in range(1, k+1): print(" "*(i) + "#"*((2*k-1)-(2*i)))
 input()
